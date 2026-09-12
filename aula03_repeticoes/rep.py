95c06649c1b319d121400aa2e862e7377f42740d
Matheus Camargo <theus.xalmeida@gmail.com>
2026-09-12 00:31:58 -0300

    estrutura?repeticoes/while/for

---
+++ aula03_repeticoes/rep.py
@@ -0,0 +1,26 @@
+# numeros = [1, 2, 3, 4, 5]
+
+
+
+
+ # for numero in numeros:
+#   print("O número", numero, "vezes 2 é igual a:", numero * 2)
+
+
+
+
+
+while numero != 0:
+
+ 
+ numero = int(input())
+
+ if numero % 2 == 0:
+
+  print("par")
+
+else:
+
+   print("impar")
+
+numero = int(input())

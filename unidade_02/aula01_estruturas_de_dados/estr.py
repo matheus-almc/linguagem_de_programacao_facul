e05820ed4622167edeade171ddd120d0a656822b
Matheus Camargo <theus.xalmeida@gmail.com>
2026-09-19 21:47:17 -0300

    estruturara de dados em python facul

---
+++ unidade_02/aula01_estruturas_de_dados/estr.py
@@ -0,0 +1,8 @@
+texto = "Explorando a diversidade de linguagens de programação com Pyhton. matheus almeda egg"
+print(f"Tamanho do texto: {len(texto)}")
+
+print(f"Python in texto: {'python' in texto}")
+
+print(F"quantidade de do mesmo elemento no texto: {texto.count('o')}")
+
+print(f"as 5 primeiras letras sao: {texto[:5]}")

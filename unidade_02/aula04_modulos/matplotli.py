38fd0baa88c234d69b0087aa0c073955e183da99
Matheus Camargo <theus.xalmeida@gmail.com>
2026-09-23 22:19:43 -0300

    modulos em python

---
+++ unidade_02/aula04_modulos/matplotli.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+
+y = [2, 4, 1, 3, 5]
+
+plt.plot(x, y)
+
+plt.xlabel('Eixo X')
+plt.ylabel('Eixo Y')
+
+plt.title('Exemplo de Gráfico de Linha')
+
+plt.show()
---
+++ unidade_02/aula04_modulos/modulos.py
@@ -0,0 +1,20 @@
+#primeiro modo
+
+import math
+math.sqrt(25)
+math.log2(1024)
+math.cos(45)
+
+#segundo modo
+
+import math as m
+m.sqrt(25)
+m.log2(1024)
+m.cos(45)
+
+#terceiro modo
+
+from math import sqrt, log2, cos
+sqrt(25)
+log2(1024)
+cos(45)
